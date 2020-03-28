0449a5cb604e72604b141d19969d1aba9a45d729
Sergey Simakin <sergey.simakin98@gmail.com>
2020-03-28 18:03:57 +0200

    registration form

--- WebCalendar/src/WebCalendar.WebApi/ClientApp/src/app/app-routing.module.ts
+++ WebCalendar/src/WebCalendar.WebApi/ClientApp/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {MainLayoutComponent} from "./shared/components/main-layout/main-layout.component";
+import {HomePageComponent} from "./home-page/home-page.component";
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {RegistrationPageComponent} from "./registration-page/registration-page.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: HomePageComponent},
+      {path: 'login', component: LoginPageComponent},
+      {path: 'registration', component: RegistrationPageComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WebCalendar/src/WebCalendar.WebApi/ClientApp/src/app/app.module.ts
+++ WebCalendar/src/WebCalendar.WebApi/ClientApp/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    RegistrationPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
