a88cbdef519efa78fc2954a0d78639497732919c
Sergey Simakin <sergey.simakin98@gmail.com>
2020-04-12 00:07:07 +0300

    add email sender service

---
+++ WebCalendar/src/WebCalendar.Services.EmailSender/Contracts/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using WebCalendar.Services.EmailSender.Models;
+
+namespace WebCalendar.Services.EmailSender.Contracts
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(Message message);
+    }
+}
