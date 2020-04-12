49d445be20ff03fb53bff53b208fe479f14b4dab
Sergey Simakin <sergey.simakin98@gmail.com>
2020-04-12 20:41:09 +0300

    add angular push notifications

--- WebCalendar/src/WebCalendar.WebApi/ClientApp/src/app/app.component.ts
+++ WebCalendar/src/WebCalendar.WebApi/ClientApp/src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NotificationMiddlewareService} from "./core/service/notification-middleware.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private notificationMiddleware: NotificationMiddlewareService) {
+  }
+  ngOnInit(): void {
+    this.notificationMiddleware.init();
+  }
 }
